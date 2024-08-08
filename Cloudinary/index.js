5cea4df7db65d31d967f9572edb20670127beff5
Saksham Chhabra <154679424+Saksham-Chhabra@users.noreply.github.com>
2024-08-08 17:55:57 +0530

    Update index.js

--- Cloudinary/index.js
+++ Cloudinary/index.js
@@ -1,14 +1,14 @@
-const Cloudinary = require('Cloudinary').v2;
+const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-Cloudinary.config({
+cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
 });
 
 const storage = new CloudinaryStorage({
-    Cloudinary,
+    cloudinary,
     params: {
         folder: 'YelpCamp',
         allowedFormats: ['jpeg', 'png', 'jpg']
@@ -16,6 +16,6 @@ const storage = new CloudinaryStorage({
 });
 
 module.exports = {
-    Cloudinary,
+    cloudinary,
     storage
 }
