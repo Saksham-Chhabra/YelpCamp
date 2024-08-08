b98b2032773248de81108efeafd7c07ee7e4e1b3
Saksham Chhabra <154679424+Saksham-Chhabra@users.noreply.github.com>
2024-08-08 17:50:50 +0530

    Update index.js

--- Cloudinary/index.js
+++ Cloudinary/index.js
@@ -1,4 +1,4 @@
-const cloudinary = require('cloudinary').v2;
+const Cloudinary = require('Cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
